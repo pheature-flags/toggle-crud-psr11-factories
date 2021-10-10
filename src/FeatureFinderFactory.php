40e958abf10cbd276710c33f1903d549870578c7
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-10-10 13:15:53 +0200

    [#4] allow creating chain feature finder

--- src/FeatureFinderFactory.php
+++ src/FeatureFinderFactory.php
@@ -6,6 +6,7 @@ namespace Pheature\Crud\Psr11\Toggle;
 
 use Doctrine\DBAL\Connection;
 use InvalidArgumentException;
+use Pheature\Core\Toggle\Read\ChainFeatureFinder;
 use Pheature\Core\Toggle\Read\ChainToggleStrategyFactory;
 use Pheature\Core\Toggle\Read\FeatureFinder;
 use Pheature\Dbal\Toggle\Read\DbalFeatureFactory;
@@ -23,14 +24,16 @@ final class FeatureFinderFactory
         $config = $container->get(ToggleConfig::class);
         /** @var ChainToggleStrategyFactory $chainToggleStrategyFactory */
         $chainToggleStrategyFactory = $container->get(ChainToggleStrategyFactory::class);
-
-        if (ToggleConfig::DRIVER_DBAL === $config->driver()) {
+        $connection = null;
+        if (
+            ToggleConfig::DRIVER_DBAL === $config->driver()
+            || true === in_array(ToggleConfig::DRIVER_DBAL, $config->driverOptions(), true)
+        ) {
             /** @var ?Connection $connection */
             $connection = $container->get(Connection::class);
-            return self::create($config, $chainToggleStrategyFactory, $connection);
         }
 
-        return self::create($config, $chainToggleStrategyFactory, null);
+        return self::create($config, $chainToggleStrategyFactory, $connection);
     }
 
     public static function create(
@@ -55,6 +58,26 @@ final class FeatureFinderFactory
             return new DbalFeatureFinder($connection, new DbalFeatureFactory($chainToggleStrategyFactory));
         }
 
+        if (ToggleConfig::DRIVER_CHAIN === $driver) {
+            $drivers = [];
+            foreach ($config->driverOptions() as $driverOption) {
+                $driverConfig = self::getDriverConfig($driverOption, $config);
+                $drivers[] = self::create($driverConfig, $chainToggleStrategyFactory, $connection);
+            }
+
+            return new ChainFeatureFinder(...$drivers);
+        }
+
         throw new InvalidArgumentException('Valid driver required');
+    }
+
+    private static function getDriverConfig(string $driverOption, ToggleConfig $config): ToggleConfig
+    {
+        return new ToggleConfig([
+            'driver' => $driverOption,
+            'api_enabled' => $config->apiEnabled(),
+            'api_prefix' => $config->apiPrefix(),
+            'toggles' => $config->toggles(),
+        ]);
     }
 }
--- test/FeatureFinderFactoryTest.php
+++ test/FeatureFinderFactoryTest.php
@@ -3,6 +3,7 @@
 namespace Pheature\Test\Crud\Psr11\Toggle;
 
 use Doctrine\DBAL\Connection;
+use Pheature\Core\Toggle\Read\ChainFeatureFinder;
 use Pheature\Core\Toggle\Read\ChainToggleStrategyFactory;
 use Pheature\Core\Toggle\Read\FeatureFinder;
 use Pheature\Core\Toggle\Read\SegmentFactory;
@@ -20,6 +21,7 @@ class FeatureFinderFactoryTest extends TestCase
     public function testItShouldThrowAnExceptionWhenItCantCreateAFeatureFinder(): void
     {
         $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Expected one of: "inmemory", "dbal", "chain". Got: "some_other"');
         $container = $this->createMock(ContainerInterface::class);
         $chainStrategyFactory = new ChainToggleStrategyFactory(
             $this->createMock(SegmentFactory::class),
@@ -36,7 +38,6 @@ class FeatureFinderFactoryTest extends TestCase
 
         $featureFinderFactory->__invoke($container);
     }
-
 
     public function testItShouldCreateADBalFeatureFinderFromInvokable(): void
     {
@@ -79,6 +80,27 @@ class FeatureFinderFactoryTest extends TestCase
         self::assertInstanceOf(InMemoryFeatureFinder::class, $finder);
     }
 
+    public function testItShouldCreateAChainFeatureFinderFromInvokable(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $toggleConfig = new ToggleConfig(['api_enabled' => false, 'api_prefix' => '', 'driver' => 'chain', 'driver_options' => ['dbal', 'inmemory']]);
+        $chainStrategyFactory = new ChainToggleStrategyFactory(
+            $this->createMock(SegmentFactory::class),
+            $this->createMock(ToggleStrategyFactory::class),
+        );
+        $connection = $this->createMock(Connection::class);
+
+        $container->expects(self::exactly(3))
+            ->method('get')
+            ->withConsecutive([ToggleConfig::class], [ChainToggleStrategyFactory::class], [Connection::class])
+            ->willReturnOnConsecutiveCalls($toggleConfig, $chainStrategyFactory, $connection);
+
+        $featureFinderFactory = new FeatureFinderFactory();
+
+        $finder = $featureFinderFactory->__invoke($container);
+        self::assertInstanceOf(ChainFeatureFinder::class, $finder);
+    }
+
     public function testItShouldCreateADBalFeatureFinderFromCreate(): void
     {
         $toggleConfig = new ToggleConfig(['api_enabled' => false, 'api_prefix' => '', 'driver' => 'dbal']);
@@ -91,9 +113,36 @@ class FeatureFinderFactoryTest extends TestCase
         self::assertInstanceOf(FeatureFinder::class, $featureFinder);
     }
 
+    public function testItShouldThrowExceptionWithInvalidFeatureFinderFromCreate(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Valid driver required');
+        $toggleConfig = new ToggleConfig(['api_enabled' => false, 'api_prefix' => '', 'driver' => 'inmemory']);
+        $reWriteConfig = fn () => $this->driver = 'some_other';
+        $reWriteConfig->call($toggleConfig);
+        $chainStrategyFactory = new ChainToggleStrategyFactory(
+            $this->createMock(SegmentFactory::class),
+            $this->createMock(ToggleStrategyFactory::class),
+        );
+        $connection = $this->createMock(Connection::class);
+        FeatureFinderFactory::create($toggleConfig, $chainStrategyFactory, $connection);
+    }
+
     public function testItShouldCreateInMemoryFeatureFinderFromCreate(): void
     {
         $toggleConfig = new ToggleConfig(['api_enabled' => false, 'api_prefix' => '', 'driver' => 'inmemory']);
+        $chainStrategyFactory = new ChainToggleStrategyFactory(
+            $this->createMock(SegmentFactory::class),
+            $this->createMock(ToggleStrategyFactory::class),
+        );
+        $connection = null;
+        $featureFinder = FeatureFinderFactory::create($toggleConfig, $chainStrategyFactory, $connection);
+        self::assertInstanceOf(FeatureFinder::class, $featureFinder);
+    }
+
+    public function testItShouldCreateChainFeatureFinderFromCreate(): void
+    {
+        $toggleConfig = new ToggleConfig(['api_enabled' => false, 'api_prefix' => '', 'driver' => 'chain', 'driver_options' => ['inmemory']]);
         $chainStrategyFactory = new ChainToggleStrategyFactory(
             $this->createMock(SegmentFactory::class),
             $this->createMock(ToggleStrategyFactory::class),
