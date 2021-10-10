3e316d53e384180d97c6b27417b685d48816a4d2
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-10-10 15:18:07 +0200

    [#12] support ChainFeatureRepository

--- src/FeatureRepositoryFactory.php
+++ src/FeatureRepositoryFactory.php
@@ -6,6 +6,7 @@ namespace Pheature\Crud\Psr11\Toggle;
 
 use Doctrine\DBAL\Connection;
 use InvalidArgumentException;
+use Pheature\Core\Toggle\Write\ChainFeatureRepository;
 use Pheature\Core\Toggle\Write\FeatureRepository;
 use Pheature\Dbal\Toggle\Write\DbalFeatureRepository;
 use Pheature\InMemory\Toggle\InMemoryFeatureRepository;
@@ -36,6 +37,25 @@ final class FeatureRepositoryFactory
             return new DbalFeatureRepository($connection);
         }
 
+        if (ToggleConfig::DRIVER_CHAIN === $driver) {
+            $drivers = [];
+            foreach ($config->driverOptions() as $driverOption) {
+                $drivers[] = self::create(self::getDriverConfig($driverOption, $config), $connection);
+            }
+
+            return new ChainFeatureRepository(...$drivers);
+        }
+
         throw new InvalidArgumentException('Valid driver required');
+    }
+
+    private static function getDriverConfig(string $driverOption, ToggleConfig $config): ToggleConfig
+    {
+        return new ToggleConfig([
+            'driver' => $driverOption,
+            'api_enabled' => $config->apiEnabled(),
+            'api_prefix' => $config->apiPrefix(),
+            'toggles' => $config->toggles(),
+        ]);
     }
 }
--- test/FeatureRepositoryFactoryTest.php
+++ test/FeatureRepositoryFactoryTest.php
@@ -3,6 +3,7 @@
 namespace Pheature\Test\Crud\Psr11\Toggle;
 
 use Doctrine\DBAL\Connection;
+use Pheature\Core\Toggle\Write\ChainFeatureRepository;
 use Pheature\Core\Toggle\Write\FeatureRepository;
 use Pheature\Crud\Psr11\Toggle\FeatureFinderFactory;
 use Pheature\Crud\Psr11\Toggle\FeatureRepositoryFactory;
@@ -64,6 +65,23 @@ class FeatureRepositoryFactoryTest extends TestCase
 
         $repository = $featureRepositoryFactory->__invoke($container);
         self::assertInstanceOf(InMemoryFeatureRepository::class, $repository);
+    }
+
+    public function testItShouldCreateAChainFeatureRepositoryFromInvokable(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $toggleConfig = new ToggleConfig(['api_enabled' => false, 'api_prefix' => '', 'driver' => 'chain', 'driver_options' => ['dbal']]);
+        $connection = $this->createMock(Connection::class);
+
+        $container->expects(static::exactly(2))
+            ->method('get')
+            ->withConsecutive([ToggleConfig::class], [Connection::class])
+            ->willReturnOnConsecutiveCalls($toggleConfig, $connection);
+
+        $featureRepositoryFactory = new FeatureRepositoryFactory();
+
+        $repository = $featureRepositoryFactory->__invoke($container);
+        self::assertInstanceOf(ChainFeatureRepository::class, $repository);
     }
 
     public function testItShouldCreateADBalFeatureRepositoryFromCreate(): void
